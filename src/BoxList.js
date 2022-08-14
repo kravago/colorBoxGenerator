1a497b697ca8494da4acce0e4a7acb43f3baca4a
Engines-screaming <karl.ravago@gmail.com>
2022-08-13 17:49:17 -0700

    boxes render

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import Box from './Box';
-import NewBoxForm from './NewBoxForm';
+// import NewBoxForm from './NewBoxForm';
 
 const BoxList = () => {
-    const { boxes, setBox } = useState([]);
+    const [boxes, setBoxes] = useState(['blue', 'red', 'black']);
+    // const createBox = (color) => { setBox([...boxes, color]) }
+
+    console.log(boxes);
 
     return (
         <div>
-            <NewBoxForm />
-            {boxes.map((box, i) => (
-                <Box color={box} key={i} />
+            {boxes.map((box) => (
+                <Box color={box} />
             ))}
         </div>
     )
 }
+
+export default BoxList;
